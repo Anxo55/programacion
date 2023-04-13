d5d173f1661fdc9fd4b1fa58aa82d8e06f4db633
Anxo Campos <anxo.campos260204@gmail.com>
2023-04-13 14:42:23 +0200

    all

--- 2022-2023/Anxito/3ªEV/Objetos/Maquina/Control.java
+++ 2022-2023/Anxito/3ªEV/Objetos/Maquina/Control.java
@@ -1,122 +1,126 @@
 package Maquina;
 
+import java.util.Scanner;
+
 public class Control {
-    // ***************************************************
-4 // * Recoge monedas en 'Maquina' para cobrar 'Precio'.
+// ***************************************************
+ // * Recoge monedas en 'Maquina' para cobrar 'Precio'.
 // * Devuelve 'true'
-5 // * si el pago se ha realizado con exito y 'false' en
+ // * si el pago se ha realizado con exito y 'false' en
 // * caso contrario
-6 // ****************************************************
-7 public static boolean IntroduccionMonedas
-8 (MaquinaModeloSencillo Maquina, float Precio) {
-9
-10 String Accion;
-11 char Car;
-12 boolean Pagado=false, Anulado = false, CambioOK ;
-13 float Acumulado = 0;
-14
-15 do {
-16 System.out.println("-- u,d,a --");
-17 Accion = Teclado.Lee_String();
-18 Car = Accion.charAt(0);
-19 switch (Car) {
-20 case 'u':
-21 if (Maquina.Deposito1Euro.HayHueco()) {
+ // ****************************************************
+ public static boolean IntroduccionMonedas (Modelo Maquina, float Precio) {
 
-ã BOBADILLA CAPÍTULO 4: PROGRAMACIÓN ORIENTADA A OBJETOS USANDO CLASES 131
+Scanner sc = new Scanner(System.in);
+ String Accion;
+ char Car;
+ //declaracion de booleanos pagar y o anular
+ boolean Pagado=false, Anulado = false;
+ float Acumulado = 0;
 
-22 Maquina.Deposito1Euro.MeteElemento();
-23 Acumulado = Acumulado + 1f;
-24 } else
-25 System.out.println("Temporalmente esta
-26 maquina no cepta monedas de un euro");
-27 break;
-28
-29 case 'd':
-30 if (Maquina.Deposito01Euro.HayHueco()) {
-31 Maquina.Deposito01Euro.MeteElemento();
-32 Acumulado = Acumulado + 0.1f;
-33 } else
-34 System.out.println("Temporalmente esta
-35 maquina no acepta monedas de 0.1 euros");
-36 break;
-37
-38 case 'a':
-39 System.out.println("Operación anulada");
-40 Anulado = true;
-41 break;
-42 }
-43
-44 Maquina.MostrarEstadoMaquina();
-45
-46 } while (Acumulado<Precio || Anulado);
-47
-48 if (Anulado)
-49 Devolver(Maquina,Acumulado);
-50 else
-51 if (CambioDisponible(Maquina,Acumulado-Precio)) {
-52 Devolver (Maquina,Acumulado-Precio);
-53 } else {
-54 System.out.println("La maquina no dispone del
-cambio necesario");
-55 Devolver(Maquina,Acumulado);
-56 Anulado = true;
-57 }
-58 return (!Anulado);
-59 }
-60
-61
-62
-63 // ******************************************************
-64 // * Indica si es posible devolver 'Cantidad' euros en
+ do {
+ System.out.println("-- u(mete 1 Euro),d(mete 10 Centimos),a(Anula Operacion) --");
+ Accion = sc.nextLine();
+ Car = Accion.charAt(0);
+ switch (Car) {
+
+//PRIMER CASO
+ case 'u':
+ if (Maquina.Deposito1Euro.HayHueco()) {
+//pillamos un euro
+ Maquina.Deposito1Euro.MeteElemento();
+ Acumulado = Acumulado + 1f;
+ } else
+ System.out.println("Temporalmente esta maquina no cepta monedas de un euro");
+ break;
+
+ //SEGUNDO CASO
+ case 'd':
+     if (Maquina.Deposito01Euro.HayHueco()) {
+        //pilla diez centimos
+    Maquina.Deposito01Euro.MeteElemento();
+    Acumulado = Acumulado + 0.1f;
+ } else
+    System.out.println("Temporalmente esta maquina no acepta monedas de 0.1 euros");
+ break;
+
+//TERCER CASO
+ case 'a':
+ System.out.println("Operación anulada");
+ Anulado = true;
+ break;
+ }
+
+ Maquina.MostrarEstadoMaquina();
+
+ } while (Acumulado < Precio || Anulado);
+
+ if (Anulado)
+ Devolver(Maquina,Acumulado);
+ else
+ if (CambioDisponible(Maquina,Acumulado-Precio)) {
+ Devolver (Maquina,Acumulado-Precio);
+ System.out.println("Recoja su cambio");
+ } else {
+ System.out.println("La maquina no dispone del cambio necesario");
+ Devolver(Maquina,Acumulado);
+ Anulado = true;
+ }
+ 
+ return (!Anulado);
+ }
+
+
+
+
+
+ // ******************************************************
+ // * Indica si es posible devolver 'Cantidad' euros en
 // * 'Maquina'
-65 // ******************************************************
-66 private static boolean CambioDisponible
-67 (MaquinaModeloSencillo Maquina, float Cantidad) {
+// ******************************************************
+ private static boolean CambioDisponible(Modelo Maquina, float Cantidad) {
+int Monedas1,Monedas01;
 
-132 JAVA A TRAVÉS DE EJEMPLOS Ó JESÚS BOBADILLA SANCHO (JBOBI@EUI.UPM.ES)
+ Cantidad = Cantidad + 0.01f; //Evita problemas de falta de precision
+ //sabe cuantas de un euro
+ Monedas1 = (int) Math.floor((double) Cantidad);
+ //resta las de un euro
+ Cantidad = Cantidad - (float) Monedas1;
+ //cantidad la multiplica por 10 (10 veces 10 centimos hacen un euro)
+ Monedas01 = (int) Math.floor((double) Cantidad*10f);
+ return 
+(Maquina.Deposito1Euro.getNumeroDeElementos()>=Monedas1)&&
+(Maquina.Deposito01Euro.getNumeroDeElementos()>=Monedas01);
+ }
 
-68
-69 int Monedas1,Monedas01;
-70
-71 Cantidad = Cantidad + 0.01f; //Evita problemas de
+ 
+
+
+ // **************************************************
+ // * Devuelve la cantidad de dinero indicada,
+ // * actualizando los almacenes de monedas
+ // **************************************************
+ private static void Devolver (Modelo Maquina, float Cantidad) {
+
+ int Monedas1,Monedas01;
+ Cantidad = Cantidad + 0.01f; //Evita problemas de
 //falta de precision
-72 Monedas1 = (int) Math.floor((double) Cantidad);
-73 Cantidad = Cantidad - (float) Monedas1;
-74 Monedas01 = (int) Math.floor((double) Cantidad*10f);
-75 return {
-(Maquina.Deposito1Euro.DimeNumeroDeElementos()>=Monedas1)&&
-(Maquina.Deposito01Euro.DimeNumeroDeElementos()>=Monedas01));
-76 }
-77
-78
-79
-80 // **************************************************
-81 // * Devuelve la cantidad de dinero indicada,
-82 // * actualizando los almacenes de monedas
-83 // **************************************************
-84 private static void Devolver (MaquinaModeloSencillo
-85 Maquina, float Cantidad) {
-86
-87 int Monedas1,Monedas01;
-88 Cantidad = Cantidad + 0.01f; //Evita problemas de
-//falta de precision
-89 Monedas1 = (int) Math.floor((double)Cantidad);
-90 Cantidad = Cantidad - (float) Monedas1;
-91 Monedas01 = (int) Math.floor((double)Cantidad*10f);
-92
-93 for (int i=1;i<=Monedas1;i++){
-94 Maquina.Deposito1Euro.SacaElemento();
-95 // Sacar 1 moneda de un euro
-96 }
-97
-98 for (int i=1;i<=Monedas01;i++){
-99 Maquina.Deposito01Euro.SacaElemento();
-100 // Sacar 1 moneda de 0.1 euro
-101 }
-102 System.out.println("Recoja el importe: "+Monedas1+"
-103 monedas de un euro y "+Monedas01+
-104 " monedas de 0.1 euros");
-105
-106 }
+ Monedas1 = (int) Math.floor((double)Cantidad);
+ Cantidad = Cantidad - (float) Monedas1;
+ Monedas01 = (int) Math.floor((double)Cantidad*10f);
+
+ for (int i=1;i<=Monedas1;i++){
+ Maquina.Deposito1Euro.SacaElemento();
+ // Sacar 1 moneda de un euro
+ }
+
+ for (int i=1;i<=Monedas01;i++){
+ Maquina.Deposito01Euro.SacaElemento();
+ // Sacar 1 moneda de 0.1 euro
+ }
+ System.out.println("Recoja el importe: "+Monedas1+"monedas de un euro y "+Monedas01+
+ " monedas de 0.1 euros");
+
+ }
 }
+
---
+++ 2022-2023/Anxito/3ªEV/Objetos/Maquina/testMaquina.java
@@ -0,0 +1,81 @@
+package Maquina;
+
+import java.util.Scanner;
+
+public class testMaquina {
+    public static void main(String[] args){
+        
+        String Accion;
+        char Car;
+        Scanner sc = new Scanner(System.in);
+
+        Modelo MiMaquina = new
+        Modelo();
+         MiMaquina.PonPrecios(1.1f, 1.3f, 0.8f);
+         MiMaquina.Deposito1Euro.MeteElemento();
+         MiMaquina.Deposito01Euro.MeteElemento();
+         MiMaquina.Deposito01Euro.MeteElemento();
+         MiMaquina.DepositoCocaCola.RellenaAlmacen();
+         MiMaquina.DepositoNaranja.RellenaAlmacen();
+         //MiMaquina.Deposito1Euro.RellenaAlmacen(); No nos
+         // ha llegado el suministro de agua
+        
+         do {
+         System.out.println("-- c,n,a,s --");
+         Accion = sc.nextLine();
+         Car = Accion.charAt(0);
+         switch (Car) {
+         case 'c':
+         System.out.println("Ha seleccionado Coca cola");
+         if(MiMaquina.DepositoCocaCola.HayElemento()) {
+         if (Control.IntroduccionMonedas
+         (MiMaquina,MiMaquina.DimePrecioCocaCola())) {
+         MiMaquina.DepositoCocaCola.SacaElemento();
+         System.out.println("No olvide coger su cocacola");
+         // Sacar fisicamente la CocaCola
+         }
+         }
+         else
+         System.out.println("Producto agotado");
+         break;
+        
+         case 'n':
+         System.out.println("Ha seleccionado Naranja");
+         if (MiMaquina.DepositoNaranja.HayElemento()) {
+         if (Control.IntroduccionMonedas
+         (MiMaquina,MiMaquina.DimePrecioNaranja())) {
+         MiMaquina.DepositoNaranja.SacaElemento();
+         System.out.println("No olvide coger su naranja");
+         // Sacar fisicamente la Naranja
+         }
+         }
+         else
+         System.out.println("Producto agotado");
+         break;
+        
+         case 'a':
+         System.out.println("Ha seleccionado Agua");
+         if (MiMaquina.DepositoAgua.HayElemento()) {
+         if (Control.IntroduccionMonedas (MiMaquina, MiMaquina.DimePrecioAgua())) {
+         MiMaquina.DepositoAgua.SacaElemento();
+         System.out.println("No olvide coger su agua");
+         // Sacar fisicamente el agua
+         }
+         }
+         else
+         System.out.println("Producto agotado");
+         break;
+
+         case 's':
+         System.out.println("fin de la simulacion");
+         
+        
+         default:
+         System.out.println("Error de seleccion, intentelo de nuevo");
+         break;
+         }
+         MiMaquina.MostrarEstadoMaquina();
+        
+         } while (!Accion.equals("s"));
+         }
+         }
