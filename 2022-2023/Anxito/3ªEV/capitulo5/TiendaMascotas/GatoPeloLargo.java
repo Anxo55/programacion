c9a9bd0902e40d15b342d07d18c20bf52ef6950e
Anxo Campos <anxo.campos260204@gmail.com>
2023-05-02 14:50:22 +0200

    all

--- 2022-2023/Anxito/3ªEV/capitulo5/TiendaMascotas/GatoPeloLargo.java
+++ 2022-2023/Anxito/3ªEV/capitulo5/TiendaMascotas/GatoPeloLargo.java
@@ -2,11 +2,20 @@ package capitulo5.TiendaMascotas;
 
 public class GatoPeloLargo extends Gatos{
 
+public GatoPeloLargo(String nombre, int edad, String color, float altura, float longitudSalto) {
+        super(nombre, edad, color, altura, longitudSalto);
+        /this.raza=raza;
+    }
+
+
+
 enum razas 
 {ANGORA, MALAYO, BALINES,SOMALI}    
 
 //atributo
 private razas  raza;
+
+
 
 @Override
 public String toString() {
