901e8b05d8f2f1261670cef6deab6d991dc60dcf
Anxo Campos <anxo.campos260204@gmail.com>
2023-05-09 09:41:03 +0200

    all

--- 2022-2023/Anxito/3ªEV/capitulo5/ejemplo48/Test.java
+++ 2022-2023/Anxito/3ªEV/capitulo5/ejemplo48/Test.java
@@ -2,10 +2,9 @@ public class Test {
 
     public static void main(String[] args) {
         Equipo equipo1 = new Equipo("Sky", "Estados Unidos");
-
         Velocista velocista1 = new Velocista(123979, "Geraint Thomas", 320,25);
         Escalador escalador1 = new Escalador(123980, "Egan Bernal", 25, 10);
-        Contrarrelojista contrarrelojista = new Contrarrelojista(123981, "Jonathan Castroviejo", 0, 0);
+        Contrarrelojista contrarrelojista = new Contrarrelojista(1502, "Jonathan Catroviejo", 120, 0, 0);
 
         equipo1.añadirCiclista(velocista1);
         equipo1.añadirCiclista(escalador1);
